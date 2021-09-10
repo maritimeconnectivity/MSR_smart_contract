4f81ad3e8a038aa558359bd364cd22c96bb07b22
Jinki Jung <your.jinki.jung@gmail.com>
2021-09-10 09:35:57 +0200

    introducing status at migration

--- migrations/2_msr_migration.js
+++ migrations/2_msr_migration.js
@@ -5,6 +5,7 @@ module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(MsrContract);
     const instance = await MsrContract.deployed();
     const MSR_ADMIN_ROLE = await instance.MSR_ADMIN_ROLE.call();
+    const initialStatus = MsrContract.InstanceStatus.Provisional;
     await instance.grantRole(MSR_ADMIN_ROLE, accounts[0], {from: accounts[0]});
     await instance.addMsr('MSR 1', 'https://example.com', accounts[0], {from: accounts[0]});
     const service = 
@@ -14,6 +15,7 @@ module.exports = async function(deployer, network, accounts) {
             version: '0.1', 
             keywords: '', 
             coverageArea: 'POLYGON((10.689 -25.092, 34.595 -20.170, 38.814 -35.639, 13.502 -39.155, 10.689 -25.092))', 
+            status: initialStatus,
             implementsDesignMRN: 'urn:mrn:mcp:service:mcc:core:design:example', 
             implementsDesignVersion: '0.1',
             msr: {
